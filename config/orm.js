1603ab9b2d40ea0cf1bf27a782cdc9fb7f05851e
Jesse Thomas <jayayetea@gmail.com>
2017-04-27 18:33:49 -0500

    adding orm functions to export

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,36 @@
 var connection = require('./config/connection.js');
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 var orm = {
 
-	
+	selectAll : function(table,cb) {
+		var queryString = "SELECT * FROM "+table+";";
+		connection.query(queryString, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+	insertOne : function(table,cols,vals,cb) {
+		var queryString = "INSERT INTO "+table+" (";
+		queryString += cols.toString()+") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ")";
 
+
+	},	
+	updateOne : function() {
+
+	}
 };
 
 module.exports = orm;
